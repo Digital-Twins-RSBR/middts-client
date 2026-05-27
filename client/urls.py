b6e2a5fd01daa4b672d370e88a950ab3a646167c
Lucas <lucasilpe@gmail.com>
2026-05-27 23:41:19 +0000

    fix(client): serve /static/ from STATIC_ROOT in urls for containerized access

--- client/urls.py
+++ client/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("", include("twins.urls")),  # Inclui as URLs do app twins
     path('admin/', admin.site.urls),
+]
+
+# Serve static files via Django when running in containerized setups
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
